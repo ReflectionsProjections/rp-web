e5397e177b0bc229488e5fdf3f1704f586cdcc2d
caupcakes <78387392+caupcakes@users.noreply.github.com>
2024-06-17 21:19:51 -0500

    demo push

--- src/routes/pages/Events.tsx
+++ src/routes/pages/Events.tsx
@@ -7,11 +7,68 @@ import {
   CardFooter,
   Flex,
   Grid,
-  Heading,
-  Stack,
+  Heading, Input, Modal, ModalBody, ModalCloseButton, ModalContent, ModalFooter, ModalHeader, ModalOverlay, Select,
+  Stack, Textarea, useDisclosure,
 } from '@chakra-ui/react';
 import rpLogo from '../../assets/rp_logo.png'
 import { EditIcon } from '@chakra-ui/icons';
+
+function ManualClose() {
+  const { isOpen, onOpen, onClose } = useDisclosure()
+
+  return (
+    <>
+      <Button leftIcon={<EditIcon/>} colorScheme="teal" variant="solid" onClick={onOpen}>
+        Edit
+      </Button>
+      <Modal closeOnOverlayClick={false} isOpen={isOpen} onClose={onClose}>
+        <ModalOverlay />
+        <ModalContent>
+          <ModalHeader>Edit event</ModalHeader>
+          <ModalCloseButton />
+          <ModalBody pb={6}>
+            <Input
+              placeholder="Event Name"
+              value=""
+              mb={4}
+            />
+            <Select
+              placeholder="Select Event Type"
+              value=""
+              mb={4}
+            >
+              <option value="Virtual">Virtual</option>
+              <option value="In-Person">In-Person</option>
+            </Select>
+            <Input
+              type="datetime-local"
+              value=""
+              mb={4}
+            />
+            <Input
+              placeholder="Points"
+              value=""
+              mb={4}
+            />
+            <Textarea
+              placeholder="Description"
+              value=""
+              mb={4}
+            />
+          </ModalBody>
+
+          <ModalFooter>
+            <Button colorScheme='blue' mr={3}>
+                Save
+            </Button>
+            <Button onClick={onClose}>Cancel</Button>
+          </ModalFooter>
+        </ModalContent>
+      </Modal>
+    </>
+  )
+}
+
 function Events() {
     
   return (
@@ -39,9 +96,7 @@ function Events() {
             </Stack>
           </CardBody>
           <CardFooter>
-            <Button leftIcon={<EditIcon/>} colorScheme="teal" variant="solid">
-            Edit
-            </Button>
+            {ManualClose()}
           </CardFooter>
         </Card>
 
