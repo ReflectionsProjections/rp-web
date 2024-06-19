fea5edb7f0d330c13dc80901d7c28178a04cedd3
anandani4136 <ra7353@hotmail.com>
2024-06-18 23:30:41 -0500

    Fixed background fill issue

--- src/routes/Home.tsx
+++ src/routes/Home.tsx
@@ -183,7 +183,7 @@ export default function Home() {
           ) : null}
         </Box>
 
-        <Box mt={16} flex="1" display="flex" flexDirection="column" minHeight='100%' height='120vh'>
+        <Box mt={16} flex="1" display="flex" flexDirection="column" minHeight='100vh'>
           {renderComponent()}
         </Box>
       </>
