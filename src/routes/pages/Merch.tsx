fad7bc72d285ded3dd10543039dcebb1192ddda0
divinedab <divyack2@illinois.edu>
2024-09-15 16:21:01 -0500

    still working on merch checkin

--- src/routes/pages/Merch.tsx
+++ src/routes/pages/Merch.tsx
@@ -10,36 +10,106 @@ import {
   Input,
   Switch,
   Text,
+  useToast,
 } from "@chakra-ui/react";
+import {Config} from "../../config.ts";
+import axios from "axios";
 
 function Merch() {
+  const toast = useToast();
   const [showWebcam, setShowWebcam] = useState(false); // Toggle between webcam and email input
+  const [email, setEmail] = useState("");
   const [attendeeName, setAttendeeName] = useState("Attendee Name");
-  const [merch, setMerch] = useState({
+  const [attendeePoints, setAttendeePoints] = useState(0);
+  const [hasMerch, setHasMerch] = useState({
+    Button: false,
+    Cap: false,
+    ToteBag: false,
+  });
+  const [eligibleMerch, setEligibleMerch] = useState({
     Button: false,
     Cap: false,
     ToteBag: false,
   });
 
-  const handleCheckboxChange = (e: React.ChangeEvent<HTMLInputElement>) => {
-    const { name, checked } = e.target;
-    setMerch((prevState) => ({ ...prevState, [name]: checked }));
+  const showToast = (message: string, error: boolean) => {
+    toast({
+      title: message,
+      status: error ? "error" : "success",
+      duration: 9000,
+      isClosable: true,
+    });
   };
 
-  // TODO: Write attendee search handler to call GET /attendees API endpoint
-  const handleSearch = () => {
-    console.log({
-        attendeeName,
-        merch,
-    });
-  }
+  const handleCheckboxChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    const { name, checked } = e.target;
+    setHasMerch((prevState) => ({ ...prevState, [name]: checked }));
+  };
+
+  // Get attendee merch info via their email
+  const handleSearch = async () => {
+    const jwt = localStorage.getItem("jwt");
+    if (!email) {
+      showToast('Please enter an email.', true);
+      return;
+    }
+  
+    axios
+      .get(Config.API_BASE_URL + `/attendee/${email}`, {
+        headers: {
+          Authorization: jwt,
+        },
+      })
+      .then(function (response) {
+        const merchInfo = response.data;
+  
+        // Update the state with the fetched attendee information
+        setAttendeeName(merchInfo.attendeeName);
+        setAttendeePoints(merchInfo.attendeePoints);
+        setHasMerch({
+          Button: merchInfo.hasButton,
+          Cap: merchInfo.hasCap,
+          ToteBag: merchInfo.hasTote,
+        });
+        setEligibleMerch({
+          Button: merchInfo.eligibleButton,
+          Cap: merchInfo.eligibleCap,
+          ToteBag: merchInfo.eligibleTote,
+        })
+      })
+      .catch(function () {
+        showToast('Error fetching attendee merch info.', true);
+      });
+  };
 
   // TODO: Write merch submit handler to call POST /attendees merch API endpoint
   const handleSubmit = () => {
-    console.log({
-        attendeeName,
-        merch,
-    });
+    if (eligibleMerch.Button && hasMerch.Button) {
+      const jwt = localStorage.getItem("jwt");
+      console.log("JWT")
+      console.log(jwt)
+      axios
+      .post(Config.API_BASE_URL + "/attendee/redeemMerch/Button", {
+        headers: {
+          Authorization: jwt,
+        },
+        data: {
+          email: email,
+        }
+      })
+      .then(function () {
+        showToast('Successfully updated attendee merch info!', false);
+      })
+      .catch(function () {
+        showToast('Error updating attendee merch info.', true);
+      });
+    }
+    // if (eligibleMerch.Cap) {
+
+    // }
+    // if (eligibleMerch.ToteBag) {
+
+    // }
   };
 
   return (
@@ -68,7 +138,12 @@ function Merch() {
           ) : (
             <FormControl>
               <FormLabel htmlFor="email">Enter Attendee Email</FormLabel>
-              <Input id="email" placeholder="Email" />
+              <Input
+                id="email"
+                placeholder="Email"
+                value={email}
+                onChange={(e) => setEmail(e.target.value)}
+              />
               <Button mt={4} colorScheme="blue" onClick={handleSearch}>
                 Search Attendee
               </Button>
@@ -83,25 +158,32 @@ function Merch() {
           </FormControl>
 
           <FormControl mb={4}>
+            <FormLabel htmlFor="attendee-name" textAlign="center">Points: {attendeePoints}</FormLabel>
+          </FormControl>
+
+          <FormControl mb={4}>
             <Checkbox
               name="Button"
-              isChecked={merch.Button}
+              isChecked={hasMerch.Button}
               onChange={handleCheckboxChange}
+              isDisabled={!eligibleMerch.Button}
             >
               Button
             </Checkbox>
             <Checkbox
               name="Cap"
-              isChecked={merch.Cap}
+              isChecked={hasMerch.Cap}
               onChange={handleCheckboxChange}
+              isDisabled={!eligibleMerch.Cap}
               ml={4}
             >
               Cap
             </Checkbox>
             <Checkbox
               name="ToteBag"
-              isChecked={merch.ToteBag}
+              isChecked={hasMerch.ToteBag}
               onChange={handleCheckboxChange}
+              isDisabled={!eligibleMerch.ToteBag}
               ml={4}
             >
               Tote Bag
