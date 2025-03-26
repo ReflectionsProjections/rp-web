63bef1d61ec2dbb20d95c6d48c786a630973ef8b
anandani4136 <ra7353@hotmail.com>
2025-03-26 02:35:06 -0500

    Add Attendance Page

---
+++ src/routes/pages/Attendance.tsx
@@ -0,0 +1,21 @@
+import {
+  Box,
+  Flex,
+  Heading,
+} from '@chakra-ui/react';
+    
+function Notifications() {
+      
+  return (
+    <Box flex="1" minW='90vw' p={4}>
+      <Heading size="lg">Attendance</Heading>
+      <br />
+      <Flex justify="center">
+          
+      </Flex>
+    </Box>
+  );
+}
+    
+    
+export default Notifications;
