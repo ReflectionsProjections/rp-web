6f09075403f519050800cb64347d5c88293a150a
miguelaenlle <bot.developer3@gmail.com>
2025-04-06 15:54:02 -0500

    Implemented header and exhibit sections

--- src/routes/Home.tsx
+++ src/routes/Home.tsx
@@ -1,10 +1,12 @@
 // src/routes/Home.tsx
+import { ExhibitSection } from '../pages/Home/ExhibitSection'
 import { Header } from '../pages/Home/Header'
 
 export const Home = () => {
   return (
     <>
       <Header />
+      <ExhibitSection />
     </>
   )
 }
