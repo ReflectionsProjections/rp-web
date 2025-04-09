c8ccfefb6bb605b28a48614671a6ef0f19d82bf2
share-a-byte <write2vaniramesh@gmail.com>
2025-04-09 16:16:59 -0500

    initial work on carousel

--- src/routes/Home.tsx
+++ src/routes/Home.tsx
@@ -1,10 +1,25 @@
 // src/routes/Home.tsx
-import { Header } from '../pages/Home/Header'
+import { Header } from "../pages/Home/Header";
+
+import CircularCarousel from "../components/Carousel";
+import { Box, Spacer } from "@chakra-ui/react";
+
+const teamSections = [
+  { title: "OPERATIONS", image: "/images/operations-team.jpg" },
+  { title: "DESIGN", image: "/images/design-team.jpg" },
+  { title: "DEV", image: "/images/dev-team.jpg" },
+  { title: "CONTENT", image: "/images/content-team.jpg" },
+  { title: "MARKETING", image: "/images/marketing-team.jpg" },
+];
 
 export const Home = () => {
   return (
     <>
       <Header />
+      <Box>
+        <CircularCarousel items={teamSections} />
+        <Spacer h="100px" />
+      </Box>
     </>
-  )
-}
+  );
+};
