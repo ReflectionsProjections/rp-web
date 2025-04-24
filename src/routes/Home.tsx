d5bb524935e2f2413c1ca8f717ef64f280b588dc
share-a-byte <write2vaniramesh@gmail.com>
2025-04-23 19:11:24 -0500

    initial faq fold

--- src/routes/Home.tsx
+++ src/routes/Home.tsx
@@ -1,24 +1,16 @@
 // src/routes/Home.tsx
 import { Header } from "../pages/Home/Header";
-
-import CircularCarousel from "../components/Carousel";
-import { Box, Spacer } from "@chakra-ui/react";
-
-const teamSections = [
-  { title: "OPERATIONS", image: "/images/operations-team.jpg" },
-  { title: "DESIGN", image: "/images/design-team.jpg" },
-  { title: "DEV", image: "/images/dev-team.jpg" },
-  { title: "CONTENT", image: "/images/content-team.jpg" },
-  { title: "MARKETING", image: "/images/marketing-team.jpg" },
-];
+import { Box } from "@chakra-ui/react";
+import FoldableFAQ from "../components/Foldable";
 
 export const Home = () => {
   return (
     <>
       <Header />
-      <Box>
-        <CircularCarousel items={teamSections} />
-        <Spacer h="100px" />
+      <Box minH="calc(100vh - headerHeight)">
+        {" "}
+        {/* Adjust headerHeight to your header's actual height */}
+        <FoldableFAQ />
       </Box>
     </>
   );
