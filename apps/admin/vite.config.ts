ec131b1e6731522af3f66a4629dca119e551f0e9
Jacob <edleyjacob@gmail.com>
2025-04-27 22:24:57 -0500

    configure admin site

--- apps/admin/vite.config.ts
+++ apps/admin/vite.config.ts
@@ -1,7 +1,15 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react-swc';
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react-swc";
+import { resolve } from "path";
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  envDir: resolve(__dirname, "../.."),
+  resolve: {
+    alias: {
+      "@": resolve(__dirname, "./src"),
+      "@rp/shared": resolve(__dirname, "../../shared/src")
+    }
+  }
 });
