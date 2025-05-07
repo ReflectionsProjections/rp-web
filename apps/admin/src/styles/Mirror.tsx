91c7dbde229c96cc067ae9f2396a24b91e66ba47
Jacob <edleyjacob@gmail.com>
2025-05-07 18:07:58 -0500

    adjust mirror animation

--- apps/admin/src/styles/Mirror.tsx
+++ apps/admin/src/styles/Mirror.tsx
@@ -1,6 +1,6 @@
 import { useColorModeValue } from "@chakra-ui/react";
 
-export const useMirrorStyles = () => {
+export const useMirrorStyles = (motion?: boolean) => {
   return {
     borderRadius: "2xl",
     p: 4,
@@ -18,16 +18,17 @@ export const useMirrorStyles = () => {
       left: "-85%",
       width: "50%",
       height: "100%",
-      bg: useColorModeValue("whiteAlpha.400", "whiteAlpha.200"),
+      bg: useColorModeValue("whiteAlpha.300", "whiteAlpha.200"),
       transform: "skewX(-20deg)",
-      transition: "left 0.75s ease",
       zIndex: 100000,
       pointerEvents: "none"
     },
     _hover: {
-      transform:
-        "perspective(2000px) rotateY(-1deg) rotateX(-0.5deg) scale(1.005)",
+      transform: motion
+        ? "perspective(2000px) rotateY(-1deg) rotateX(-0.5deg) scale(1.005)"
+        : "",
       _after: {
+        transition: "left 0.75s ease",
         left: "135%"
       }
     }
