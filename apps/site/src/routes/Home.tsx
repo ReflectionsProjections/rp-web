0219fca9b7dc5bccbabb189e7d71fb577562e5be
miguelaenlle <bot.developer3@gmail.com>
2025-05-19 22:24:00 -0500

    Removed schedule page content (it is on the other PR)

--- apps/site/src/routes/Home.tsx
+++ apps/site/src/routes/Home.tsx
@@ -1,11 +1,7 @@
-import Hero from "@/components/Home/Hero";
-import Schedule from "@/components/Home/Schedule";
-
 export default function Home() {
   return (
     <>
-      <Hero />
-      <Schedule />
+      <p>Reflections | Projections 2025</p>
     </>
   );
 }
--- apps/site/src/util/api.ts
+++
@@ -1,6 +0,0 @@
-import { Config } from "@/config";
-import { createApi } from "@rp/shared";
-
-const api = createApi(Config.API_BASE_URL);
-
-export default api;
