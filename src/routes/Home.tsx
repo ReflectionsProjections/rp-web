fdead1d4f650b72d7439d3f35d8ebcd2223b9b6b
anandani4136 <ra7353@hotmail.com>
2025-05-22 23:59:12 -0500

    Move component to FAQ Page

--- src/routes/Home.tsx
+++ src/routes/Home.tsx
@@ -1,17 +1,11 @@
 // src/routes/Home.tsx
 import { Header } from "../pages/Home/Header";
 import { Box } from "@chakra-ui/react";
-import FoldableFAQ from "../components/Foldable";
 
 export const Home = () => {
   return (
     <>
       <Header />
-      <Box minH="calc(100vh - headerHeight)">
-        {" "}
-        {/* Adjust headerHeight to your header's actual height */}
-        <FoldableFAQ />
-      </Box>
     </>
   );
 };
