1c641005a5eeae179015f4478d3fa89a65a87704
miguelaenlle <bot.developer3@gmail.com>
2025-07-17 20:22:35 -0500

    Ran linter

--- apps/admin/src/main.tsx
+++ apps/admin/src/main.tsx
@@ -1,5 +1,5 @@
-import { ChakraProvider } from "@chakra-ui/react";
 import React from "react";
+import { ChakraProvider } from "@chakra-ui/react";
 import ReactDOM from "react-dom/client";
 import App from "./App.tsx";
 import "./index.css";
