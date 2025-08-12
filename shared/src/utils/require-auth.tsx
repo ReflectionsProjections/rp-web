df804ce6bdbf090b43e512ac473c641b8267212d
Jacob <edleyjacob@gmail.com>
2025-08-11 20:38:25 -0500

    process of elimination

--- shared/src/utils/require-auth.tsx
+++ shared/src/utils/require-auth.tsx
@@ -1,18 +1,27 @@
 import { ReactNode } from "react";
 import { Role } from "../api/types";
-import AuthCallback from "../components/AuthCallback";
-import { Route } from "react-router-dom";
-import RequireAuthWrapper from "../components/RequireAuth";
+// import AuthCallback from "../components/AuthCallback";
+import { Outlet, Route } from "react-router-dom";
+// import RequireAuthWrapper from "../components/RequireAuth";
 
 type RequireAuthProps = {
   requiredRoles?: Role[];
   children?: ReactNode;
 };
 
-const getRequireAuth = ({ requiredRoles = [], children }: RequireAuthProps) => {
+// const getRequireAuth = ({ requiredRoles = [], children }: RequireAuthProps) => {
+//   return [
+//     <Route path="/auth/callback" element={<AuthCallback />} />,
+//     <Route element={<RequireAuthWrapper requiredRoles={requiredRoles} />}>
+//       {children}
+//     </Route>
+//   ];
+// };
+
+const getRequireAuth = ({ /*requiredRoles = [],*/ children }: RequireAuthProps) => {
   return [
-    <Route path="/auth/callback" element={<AuthCallback />} />,
-    <Route element={<RequireAuthWrapper requiredRoles={requiredRoles} />}>
+    <Route path="/auth/callback" element={<p>hi</p>} />,
+    <Route element={<Outlet />}>
       {children}
     </Route>
   ];
