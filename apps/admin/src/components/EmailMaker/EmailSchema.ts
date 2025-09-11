bc10d975c8cee8e1f69b2973f22f73e451ae1a14
seventhriver <129530873+seventhriver@users.noreply.github.com>
2025-09-11 18:10:17 -0500

    stash: adding single email recipient

--- apps/admin/src/components/EmailMaker/EmailSchema.ts
+++ apps/admin/src/components/EmailMaker/EmailSchema.ts
@@ -1,15 +1,30 @@
 import * as yup from "yup";
 
 export type EmailFormValues = {
-  recipients: string;
+  recipients?: string;
+  individualRecipient?: string;
   subject: string;
   body: string;
   isMobileNotification: boolean;
+  isIndividualEmail: boolean;
   template: string;
 };
 
 export const EmailFormSchema: yup.Schema<EmailFormValues> = yup.object({
-  recipients: yup.string().required("Recipient group is required"),
+  recipients: yup.string().when("isIndividualEmail", {
+    is: false,
+    then: (schema) =>
+      schema.required("Recipient or subscription group is required"),
+    otherwise: (schema) => schema.notRequired()
+  }),
+  individualRecipient: yup.string().when("isIndividualEmail", {
+    is: true,
+    then: (schema) =>
+      schema
+        .email("Recipient must be a valid email")
+        .required("Recipient or subscription group is required"),
+    otherwise: (schema) => schema.notRequired()
+  }),
   subject: yup
     .string()
     .required("Email subject is required")
@@ -24,13 +39,16 @@ export const EmailFormSchema: yup.Schema<EmailFormValues> = yup.object({
     }),
   body: yup.string().required("Email body is required"),
   isMobileNotification: yup.boolean().required(),
+  isIndividualEmail: yup.boolean().required(),
   template: yup.string().required()
 });
 
 export const EmailFormInitialValues: EmailFormValues = {
   recipients: "",
+  individualRecipient: "",
   subject: "",
   body: "",
   isMobileNotification: false,
+  isIndividualEmail: false,
   template: "main"
 };
