55a5548f1f650b4a7c389071efee173487a8cf40
Timothy Gonzalez <105177619+Timothy-Gonzalez@users.noreply.github.com>
2025-09-19 23:05:09 -0500

    Add websocket hook to dashboard

--- apps/dashboard/src/App.tsx
+++ apps/dashboard/src/App.tsx
@@ -14,6 +14,7 @@ import { RegisterNow } from "./components/RegisterNow";
 import { Sponsors } from "./components/Sponsors";
 import Title from "./components/Title";
 import useTimeSyncedReload from "./hooks/TimeSynchedReload";
+import WebhookPopup from "./components/WebhookPopup";
 
 const config: ThemeConfig = {
   initialColorMode: "dark",
@@ -53,6 +54,7 @@ function App() {
           paddingTop="0.5vh"
           zIndex={2}
         >
+          <WebhookPopup />
           <Title />
           <Text
             position={"absolute"}
